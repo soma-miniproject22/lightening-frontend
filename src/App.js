551d9128684bbeee12fa049fc12dd8c6801e35d9
Seongbin Kim <seongbin9786@gmail.com>
2022-04-24 11:49:43 +0900

    enhancement: Component - WantedList

--- src/App.js
+++ src/App.js
@@ -1,51 +1,62 @@
+// import { useEffect } from 'react';
+// import Api from './api/Api';
+
 import './App.css';
 import WantedList from './components/WantedList';
 
 // 이미지는 추후 백엔드/CDN에서 이미지 주소를 전달 받아야.
 import coffee from './assets/img/coffee.png';
 import kSoup from './assets/img/k-soup.png';
-// import { useEffect } from 'react';
-
-// import Api from './api/Api';
+import board2 from './assets/img/board2.jpg';
+import subway from './assets/img/subway.png';
+import walk from './assets/img/walk.jpg';
 
 const tempOriginal = [
   {
+    id: 1,
     thumbnailImage: kSoup,
-    authorName: '김성빈',
-    messageBody: `나는 배가 고프다 ! 나는 배가 고프다 ! 나는 배가 고프다 ! 나는 배가
-    고프다 ! 나는 배가 고프다 ! 나는 배가 고프다 ! 나는 배가 고프다 !
-    나는 배가 고프다 ! 나는 배가 고프다 ! 나는 배가 고프다 ! 나는 배가
-    고프다 ! 나는 배가 고프다 ! 나는 배가 고프다 ! 나는 배가 고프다 !
-    나는 배가 고프다 ! 나는 배가 고프다 ! 나는 배가 고프다 ! 나는 배가
-    고프다 ! 나는 배가 고프다 ! 나는 배가 고프다 ! 나는 배가 고프다 !
-    나는 배가 고프다 ! 나는 배가 고프다 ! 나는 배가 고프다 ! 나는 배가
-    고프다 !`,
+    authorName: '김수홍',
+    messageBody: `없어서 만드는 뜨끈한 국밥 팟`,
     beginAt: '오늘 오후 1시',
   },
   {
     id: 2,
-    thumbnailImage: coffee,
-    authorName: '김성빈',
-    messageBody: '소마 센터 주변에 맛있는 카페 탐방하러 가요',
-    beginAt: '다음 주 수요일(4/27) 오후 2시',
+    thumbnailImage: subway,
+    authorName: '이상빈',
+    messageBody: '바로 앞 서브웨이',
+    beginAt: '오늘 저녁',
   },
   {
     id: 3,
     thumbnailImage: coffee,
+    authorName: '이형창',
+    messageBody: '스벅에 같이 커피 뽑으러 가실분',
+    beginAt: '지금',
+  },
+  {
+    id: 4,
+    thumbnailImage: board2,
+    authorName: '김민겸',
+    messageBody: '보드게임 4~8인 파티 (초보만)',
+    beginAt: '오늘 저녁 먹고',
+  },
+  {
+    id: 5,
+    thumbnailImage: walk,
     authorName: '김성빈',
-    messageBody: "카페에서 객체지향 얘기하실 분 구합니다 '-'",
-    beginAt: '상시 예약 문의',
+    messageBody: '선릉 한바퀴 산책',
+    beginAt: '오늘 중',
   },
 ];
 
 // 대충 늘리기 위함 ㅠㅠ
-const tempWantedListData = [
-  ...tempOriginal,
-  ...tempOriginal.map((e) => ({ ...e, id: e.id * 2 })),
-  ...tempOriginal.map((e) => ({ ...e, id: e.id * 3 })),
-  ...tempOriginal.map((e) => ({ ...e, id: e.id * 4 })),
-  ...tempOriginal.map((e) => ({ ...e, id: e.id * 5 })),
-];
+const tempWantedListData = [0, 1, 2, 3, 4, 5].reduce((prev, idx) => {
+  const t = tempOriginal.map((e) => ({
+    ...e,
+    id: e.id + (tempOriginal.length + 1) * idx,
+  }));
+  return [...prev, ...t];
+}, []);
 
 function App() {
   // useEffect(() => {
--- src/components/WantedList.jsx
+++ src/components/WantedList.jsx
@@ -5,6 +5,23 @@ const WantedList = ({ list }) => {
   return (
     <List divided relaxed>
       {list.map(({ id, thumbnailImage, authorName, messageBody, beginAt }) => {
+        // 각 라인 별 더미 데이터 생성 로직
+        const isEyesSelected = authorName !== '김성빈' && Math.random() > 0.5;
+        const isParticipating = isEyesSelected && Math.random() > 0.5;
+        const eyesCount =
+          Math.floor(Math.random() * 5) + (isEyesSelected ? 1 : 0);
+        const handsCount =
+          Math.floor(Math.random() * 10) + (isParticipating ? 1 : 0);
+        const _names = '이상빈 김민겸 김수홍 이형창'
+          .split(' ')
+          .slice(0, Math.min(handsCount, 3)) // 3 혹은 카운터 이상으로 표시 x
+          .map((e) => (Math.random() > 0.5 ? e : '')) // 50% 확률로 선택
+          .filter((e) => !!e) // 빈 값 제외
+          .join(' '); // 문자열로 반환
+
+        const names =
+          _names + (_names.length > 0 && handsCount > 1 ? ' ...' : '');
+
         return (
           <List.Item className="b-list-item" key={id}>
             <div className="thumb-container">
@@ -14,8 +31,32 @@ const WantedList = ({ list }) => {
               <List.Header className="b-list-item-header">
                 {authorName}
               </List.Header>
-              <div>{messageBody}</div>
-              <List.Description>{beginAt}</List.Description>
+              <List.Description className="b-list-item-body">
+                {messageBody}
+              </List.Description>
+              <List.Description className="b-list-item-date">
+                {beginAt}
+              </List.Description>
+              <List.Description className="b-list-item-emoji-root-container">
+                <div
+                  className={`b-list-item-emoji-each-container${
+                    isEyesSelected ? '-selected' : ''
+                  }`}
+                >
+                  <span className="ec ec-eyes"></span>
+                  <span>{eyesCount}</span>
+                </div>
+                <div
+                  className={`b-list-item-emoji-each-container${
+                    isParticipating ? '-selected' : ''
+                  }`}
+                >
+                  <span className="ec ec-raised-hand-with-fingers-splayed"></span>
+                  <span>
+                    {handsCount} {names}
+                  </span>
+                </div>
+              </List.Description>
             </List.Content>
           </List.Item>
         );
