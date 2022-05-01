5805dafc2245e48b32a388ad1ab7f10b11a9db49
Seongbin Kim <seongbin9786@gmail.com>
2022-05-01 12:49:47 +0900

    chore: Changed Server URL, Added .env support

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,6 @@
 const config = {
-  API_SERVER: 'http://3.39.151.74/api', // api server url
-  BASE_URL: 'http://localhost:3000', // production url
+  API_SERVER: 'https://soma-back.anoldstory.com/api',
+  BASE_URL: process.env.REACT_APP_FRONTEND_URL,
 };
 
 export default config;
